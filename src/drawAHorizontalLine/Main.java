fb035adafd0b37fb2948aa5051a613fed99f7c67
Thaís Lopes <tlnloopes@gmail.com>
2018-04-30 15:07:13 -0300

    Draw a Horizontal Line

---
+++ src/drawAHorizontalLine/Main.java
@@ -0,0 +1,11 @@
+package drawAHorizontalLine;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 8;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+}
