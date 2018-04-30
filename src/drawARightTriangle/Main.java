0e175709f53914376c12858b544f1394b6beee68
Thaís Lopes <tlnloopes@gmail.com>
2018-04-30 15:12:08 -0300

    Draw a Right Triangle

---
+++ src/drawARightTriangle/Main.java
@@ -0,0 +1,14 @@
+package drawARightTriangle;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
