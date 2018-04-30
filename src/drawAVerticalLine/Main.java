214ff75cfb6b474302fb9ad7d03fd467a3b55f5d
Thaís Lopes <tlnloopes@gmail.com>
2018-04-30 15:10:44 -0300

    Draw a Vertical Line

---
+++ src/drawAVerticalLine/Main.java
@@ -0,0 +1,11 @@
+package drawAVerticalLine;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+}
