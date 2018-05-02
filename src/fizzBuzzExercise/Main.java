6d6db0fce5592adebbbd1609c2d99b48af621c5a
Thaís Lopes <tlnloopes@gmail.com>
2018-05-02 11:14:25 -0300

    FizzBuzz Exercise

---
+++ src/fizzBuzzExercise/Main.java
@@ -0,0 +1,26 @@
+package fizzBuzzExercise;
+
+public class Main {
+    public static void main(String[] args) {
+        fizzBuzz();
+    }
+
+    public static void fizzBuzz(){
+        for (int i = 1; i <= 100 ; i++) {
+
+            if((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("FizzBuzz");
+            }else {
+                if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    if (i % 3 == 0) {
+                        System.out.println("Fizz");
+                    } else {
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
+    }
+}
